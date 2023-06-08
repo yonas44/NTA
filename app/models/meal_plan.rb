fad4a91efbc6aac0bb684a8c0dcbb230a2897b24
yonas44 <tesfu.yonas1350@gmail.com>
2023-06-08 16:29:34 +0300

    Test and add create, update, destroy actions for meal_plan controller

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,5 +1,6 @@
 class MealPlan < ApplicationRecord
   belongs_to :client
   belongs_to :nutritionist
-  has_many :meal_plan_recipies, dependent: :destroy
+  has_many :meal_plan_recipes, dependent: :destroy
+  has_many :recipes, through: :meal_plan_recipes
 end
