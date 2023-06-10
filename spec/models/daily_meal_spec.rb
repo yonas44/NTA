dbfc791073da703c4dee0e998d8f32818baafd96
yonas44 <tesfu.yonas1350@gmail.com>
2023-06-10 23:12:42 +0300

    Set up daily_meal model and spec files

--- spec/models/daily_meal_spec.rb
+++ spec/models/daily_meal_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe DailyMeal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) {
+    @nutritionist = FactoryBot.create(:nutritionist, password: "password")
+    @client = FactoryBot.create(:client, password: "password")
+    @recipe = FactoryBot.create(:recipe, nutritionist_id: @nutritionist.id)
+    @meal_plan = FactoryBot.create(:meal_plan, client_id: @client.id, nutritionist_id: @nutritionist.id)
+    @meal_type = FactoryBot.create(:meal_type)
+    FactoryBot.create(:daily_meal, nutritionist_id: @nutritionist.id, recipe_id: @recipe.id, meal_plan_id: @meal_plan.id, meal_type_id: @meal_type.id)
+  }
+
+  it "returns a record of a dail_meal" do
+    expect(described_class.count).to be > 0
+  end
 end
