3a5aa1de05c7764fd550dba78797ecc32aa5faf6
yonas44 <tesfu.yonas1350@gmail.com>
2023-06-15 22:12:59 +0300

    Update nutritionist and client models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,12 +1,4 @@
 class Client < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :jwt_authenticatable,
-         :registerable,
-         jwt_revocation_strategy: JwtDenylist
-
-  has_many :meal_plans, dependent: :destroy
-  has_one :active_appointment, dependent: :destroy
+  belongs_to :user
   validates :name, presence: true
 end
--- app/models/nutritionist.rb
+++ app/models/nutritionist.rb
@@ -1,15 +1,3 @@
 class Nutritionist < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :jwt_authenticatable,
-         :registerable,
-         jwt_revocation_strategy: JwtDenylist
-
-  has_many :active_appointments, dependent: :destroy
-  has_many :clients, through: :active_appointments
-  has_many :meal_plans, dependent: :destroy
-  has_many :ingredients, dependent: :destroy
-  has_many :recipes, dependent: :destroy
-  has_many :daily_meals, dependent: :destroy
+  belongs_to :user
 end
