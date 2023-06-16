ed65afa0f331bb9dd678e9626fb313686aced659
yonas44 <tesfu.yonas1350@gmail.com>
2023-06-16 21:42:31 +0300

    Test and add users_controller

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  def create
+    user = User.new(sign_up_params)
+
+    if user.save
+      if user.role == 'nutritionist'
+        FactoryBot.create(:nutritionist, user: )
+      else
+        FactoryBot.create(:client, user: )
+      end
+      
+      yield user if block_given?
+      render json: { message: 'Signed up successfully.' }
+    else
+      clean_up_passwords resource
+      render json: { errors: user.errors.full_messages[0] }, status: :unprocessable_entity
+    end
+  end
+end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  before(:all) do
+    @user = User.create(name: 'Dr. Hamond', email: 'test@gmail.com', password: 'password', role: "nutritionst")
+    post user_session_path,
+         params: { user: { email: 'test@gmail.com', password: 'password' } }
+  end
+
+  describe 'GET /show' do
+    it 'returns the data of the user' do
+      p @user
+      get user_path(@user)
+      expect(response.body).to include 'Hamond'
+    end
+  end
+
+  describe 'PATCH /update' do
+    it 'updates the record of the signed in user' do
+      patch user_path(@user),
+            params: { user: {
+              name: "Selam Tesfu",
+              email: "selam@gmail.com",
+              password: "password",
+              role: "client",
+              client: {
+                profile_pic: "selam picture here",
+                nutritionist_id: 1
+              }
+            } }
+      expect(response.body).to include 'successfully'
+    end
+  end
+end
