95713859a6e9aa995d0ac4b2a06e537f782ae462
yonas44 <tesfu.yonas1350@gmail.com>
2023-06-16 21:43:46 +0300

    Update Factory model parameters

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    role { "client" }
   end
 end
