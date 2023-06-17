3a5372fbcbbdb05376f2c450a3664698d77533ca
yonas44 <tesfu.yonas1350@gmail.com>
2023-06-17 23:59:34 +0300

    Update user and nutritioinst model associations

--- app/models/nutritionist.rb
+++ app/models/nutritionist.rb
@@ -1,4 +1,8 @@
 class Nutritionist < ApplicationRecord
   belongs_to :user
   has_many :clients, dependent: :nullify
+  has_many :meal_plans, dependent: :destroy
+  has_many :ingredients, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+  has_many :daily_meals, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,6 @@ class User < ApplicationRecord
   
   has_one :nutritionist, dependent: :destroy
   has_one :client, dependent: :destroy
-  has_many :meal_plans, foreign_key: 'nutritionist_id', dependent: :destroy
-  has_many :ingredients, dependent: :destroy, foreign_key: 'nutritionist_id'
-  has_many :recipes, dependent: :destroy, foreign_key: 'nutritionist_id'
-  has_many :daily_meals, dependent: :destroy, foreign_key: 'nutritionist_id'
-
+  
   validates :name, :role, presence: true
 end
