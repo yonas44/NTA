31163438ecb604587fe58892a4cc09a560eec37d
yonas44 <tesfu.yonas1350@gmail.com>
2023-06-17 23:23:54 +0300

    Update cancancan exemption response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,23 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def current_auth_resource
-    if nutritionist_signed_in?
-      current_nutritionist
-    else
-      current_client
-    end
-  end
+  # def current_auth_resource
+  #   if nutritionist_signed_in?
+  #     current_nutritionist
+  #   else
+  #     current_client
+  #   end
+  # end
 
-  def current_ability
-    @current_ability = Ability.new(current_auth_resource)
+  # def current_ability
+  #   @current_ability = Ability.new(current_auth_resource)
+  # end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { message: "You are not authorized to perform this action." }, status: :unauthorized
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
   end
 end
