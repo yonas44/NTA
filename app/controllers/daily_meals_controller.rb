6e5520e7258d77c91e1d3fef92fff8302f4417f7
yonas44 <tesfu.yonas1350@gmail.com>
2023-06-19 18:06:23 +0300

    Update request handling of daily_meals controller

--- app/controllers/daily_meals_controller.rb
+++ app/controllers/daily_meals_controller.rb
@@ -1,26 +1,22 @@
 class DailyMealsController < ApplicationController
-  before_action :authenticate_nutritionist!, except: %w[index]
+  before_action :authenticate_user!
   before_action :set_daily_meal, only: %w[show update destroy]
+  load_and_authorize_resource
 
   def index
-    if current_nutritionist || current_client
-      render json: DailyMeal.where(meal_plan_id: params[:meal_plan_id]).as_json(include: :recipe)
+    if current_user.role == 'client'
+      render json: DailyMeal.where(nutritionist_id: current_user.client.nutritionist_id).as_json(include: :recipe)
     else
-      render json: { message: 'You are not authorized' }, status: 401
+      render json: DailyMeal.where(nutritionist_id: current_user.nutritionist.id).as_json(include: :recipe)
     end
   end
 
   def create
-    if MealPlan.find(params[:meal_plan_id]).nutritionist_id != current_nutritionist.id
-      return render json: { message: 'You are not authorized' },
-                    status: 401
-    end
-
     daily_meal = DailyMeal.new(daily_meal_params)
     if daily_meal.save
       render json: { message: 'Daily_meal created successfully' }, status: 200
     else
-      render json: { message: 'Daily_meal failed to create' }, status: :unprocessable_entity
+      render json: { message: daily_meal.errors.full_messages || 'Daily_meal failed to create' }, status: :unprocessable_entity
     end
   end
 
@@ -28,7 +24,7 @@ class DailyMealsController < ApplicationController
     if @daily_meal.update(daily_meal_params)
       render json: { message: 'Daily_meal updated successfully' }
     else
-      render json: { message: 'Daily_meal update failed' }, status: :unprocessable_entity
+      render json: { message: @daily_meal.errors.full_messages || 'Daily_meal update failed' }, status: :unprocessable_entity
     end
   end
 
@@ -47,8 +43,7 @@ class DailyMealsController < ApplicationController
   end
 
   def daily_meal_params
-    params.require(:daily_meal).permit(:recipe_id, :meal_type_id, :meal_date).merge(
-      nutritionist_id: current_nutritionist.id, meal_plan_id: params[:meal_plan_id]
-    )
+    params.require(:daily_meal).permit(:recipe_id, :meal_plan_id, :meal_type_id, :meal_date).merge(
+      nutritionist_id: current_user.nutritionist.id)
   end
 end
